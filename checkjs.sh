57fb3313994409269234356f00351ef631f5c2e0
Zhenjian Zhang <itdesk.zhang@gmail.com>
2021-01-14 08:39:54 +0800

    update
    Signed-off-by: Zhenjian Zhang <itdesk.zhang@gmail.com>

--- checkjs.sh
+++ checkjs.sh
@@ -192,7 +192,7 @@ description_if() {
 }
 
 task() {
-	cron_version="1.0"
+	cron_version="1.1"
 	if [ `grep -o "checkjs的定时任务$cron_version" $cron_file |wc -l` == "0" ]; then
 		echo "不存在计划任务开始设置"
 		task_delete
@@ -205,7 +205,7 @@ task() {
 }
 task_add() {
 cat >>/etc/crontabs/root <<EOF
-###########这里是checkjs的定时任务$cron_version版本###########
+#**********这里是checkjs的定时任务$cron_version版本**********#
 15 10,18,21 * * * $dir_file/checkjs.sh >/tmp/checkjs.log 2>&1
 30 21 * * * $dir_file/checkjs.sh update_script  >/tmp/checkjs_update_script.log 2>&1
 ###########102##########请将其他定时任务放到底下###############
