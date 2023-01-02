1b61a3137a65957bee9123ce6ce1499329367149
ITdesk <>
2023-01-02 21:59:00 +0800

    更新
    Signed-off-by: ITdesk <>

--- checkjs.sh
+++ checkjs.sh
@@ -1206,6 +1206,7 @@ ACTIVITY_ID				jd_wxCollectionActivity2.js
 prodevactCode				jd_prodev.js
 
 #KingRan
+jd_zsz_activityId			jd_zsz.js
 yhyauthorCode				jd_prodev.py
 jd_teamFLP_activityId			jd_teamFLP.js
 jd_showInviteJoin_activityUrl		jd_jinggeng_showInviteJoin.js
@@ -1371,7 +1372,7 @@ export jd_drawCenter_addCart="true" #// 是否做加购任务，默认不做
 							echo "$variable_script_name值：为$variable_script_num不操作"
 						else
 							case "$variable_script_name" in
-							jd_teamFLP_activityId|jd_showInviteJoin_activityUrl|jd_lzkj_loreal_invite_url|jd_wxSecond_activityId|jd_collect_shop_activityUrl|jd_collect_item_activityUrl|jd_wxKnowledgeActivity_activityUrl|jd_daily_activityId|jd_cjdaily_activityId|jd_wxCollectionActivity_activityUrl|jd_wxBuildActivity_activityId|jd_cjwxShopFollowActivity_activityId|jd_wxKnowledgeActivity_activityId|jd_cjwxKnowledgeActivity_activityId|jd_wxSecond_activityId|VENDER_ID|wish_appIdArrList|jd_mhurlList|comm_activityIDList|computer_activityId|jd_wdz_activityId|M_FOLLOW_SHOP_ARGV|VENDER_ID|PKC_TXGZYL|LUCK_DRAW_URL|DPLHTY|jd_cjhy_activityId|jd_zdjr_activityId|jd_wxShareActivity_activityId|jd_wxgame_activityId|jd_drawCenter_activityId|JD_Lottery)
+							jd_zsz_activityId|jd_teamFLP_activityId|jd_showInviteJoin_activityUrl|jd_lzkj_loreal_invite_url|jd_wxSecond_activityId|jd_collect_shop_activityUrl|jd_collect_item_activityUrl|jd_wxKnowledgeActivity_activityUrl|jd_daily_activityId|jd_cjdaily_activityId|jd_wxCollectionActivity_activityUrl|jd_wxBuildActivity_activityId|jd_cjwxShopFollowActivity_activityId|jd_wxKnowledgeActivity_activityId|jd_cjwxKnowledgeActivity_activityId|jd_wxSecond_activityId|VENDER_ID|wish_appIdArrList|jd_mhurlList|comm_activityIDList|computer_activityId|jd_wdz_activityId|M_FOLLOW_SHOP_ARGV|VENDER_ID|PKC_TXGZYL|LUCK_DRAW_URL|DPLHTY|jd_cjhy_activityId|jd_zdjr_activityId|jd_wxShareActivity_activityId|jd_wxgame_activityId|jd_drawCenter_activityId|JD_Lottery)
 								export $i
 								cp $dir_file/KingRan_Script/$js_name1 ${script_dir}/$js_name1
 								echo "${script_dir}/$js_name1运行，当前时间`date`" >>/tmp/tg_run_script.log
