e2839fd506f74a46014f2731f9f8bf27901742e1
ITdesk <>
2023-02-15 15:39:18 +0800

    修复变量错误
    Signed-off-by: ITdesk <>

--- checkjs.sh
+++ checkjs.sh
@@ -463,7 +463,7 @@ tongyong_config() {
 			git reset --hard $old_git_commit1
 			action2_num="($action2到$action3的仓库变化)"
 		elif [ "$action2_if" == "2" ];then
-			old_git_commit=$(git log --format=format:"%h" --since="$action2 00:00:00" --before="$action3 23:59:59" -1)
+			old_git_commit=$(git log --format=format:"%h" --since="$action2 00:00:00" --before="$action2 23:59:59" -1)
 			git reset --hard $old_git_commit
 			ls ./ | grep -E 'js$|py$' | sort > $Oldfile
 			action2_num="($action2到今天的仓库变化)"
